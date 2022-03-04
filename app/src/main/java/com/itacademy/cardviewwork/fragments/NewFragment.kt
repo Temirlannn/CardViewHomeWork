0919e00e06ccc2aafc76aefd1882038e1bedfc42
Temirlannn <93139917+Temirlannn@users.noreply.github.com>
2022-03-04 22:29:15 +0600

    add

--- app/src/main/java/com/itacademy/cardviewwork/fragments/NewFragment.kt
+++ app/src/main/java/com/itacademy/cardviewwork/fragments/NewFragment.kt
@@ -1,10 +1,13 @@
 package com.itacademy.cardviewwork.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.itacademy.cardviewwork.R
 import com.itacademy.cardviewwork.databinding.FragmentNewBinding
 
@@ -18,7 +21,13 @@ class NewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentNewBinding.inflate(inflater, container, false)
-
+        binding.button.setOnClickListener {
+            val dialog = SweetAlertDialog(context, SweetAlertDialog.SUCCESS_TYPE)
+            dialog.progressHelper.barColor = Color.parseColor("#A5DC86")
+            dialog.titleText = "GOOD JOB!"
+            dialog.setCancelable(false)
+            dialog.show()
+        }
         return binding.root
     }
 }
